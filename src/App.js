ade12579c0a543d09293b0bab81e22c5e6f06d87
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-03-20 23:19:05 +0100

    change file´s address

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import CreateCustomer from "./features/customers/CreateCustomer";
+import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
+
+function App() {
+  const customer = useSelector((store) => store.customer.fullName);
+
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      {customer === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
+
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
