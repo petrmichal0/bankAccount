1fb4a4d36d1b0ed2cafb214049afe79d3445318d
Petr Michal <petrmichal0@gmail.com>
2024-11-04 22:55:29 +0100

    change to vite + add gitignore #1

--- src/main.tsx
+++ src/main.tsx
@@ -12,9 +12,7 @@ console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
